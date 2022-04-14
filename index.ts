274e2df930ed1c3261a59ff8cb39e66761206135
Alexander Hill <CodeBreadpudding@gmail.com>
2022-04-13 22:58:29 -0400

    We're drawing now!

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
+import { Jimp } from "@jimp/core";
 import { PrismaClient } from ".prisma/client";
 
 import express from "express";
 import fs from "fs";
+import jimp from "jimp";
 import jwt from "jsonwebtoken";
 import ws from "ws";
 
 const app = express();
 const config = JSON.parse(fs.readFileSync("config.json").toString());
 const prisma = new PrismaClient();
+
+var image: Jimp | null = null;
+
+jimp.read("public/canvas.png").then(img => { image = img; }).catch(err => { throw err; });
 
 app.get("/", (_, res) => {
     res.sendFile(__dirname + "/public/index.html");
@@ -60,20 +66,27 @@ app.get('/api/v1/placePixel/:x/:y/:color', (req, res) => {
     // Make sure we're dealing with correct types
     if(typeof x === "number" && typeof y === "number" && typeof color === "string") {
         if(/[0-9A-Fa-f]{6}/.test(color)) {
-            // Check bounds
-            if(x >= 0 && x < 512 && y >= 0 && y < 512) {
-                // TODO: Check whether a user is authenticated or not
-                // TODO: Make sure the user hasn't placed a pixel in the last five minutes
-                // TODO: Add entry to log
-                // TODO: Update image on server
-                // Announce change via WebSocket
-                let msg = JSON.stringify({type: 2, x, y, color});
+            // Make sure we actually have an image to work with
+            if(image != null) {
+                // Check bounds
+                if(x >= 0 && x < image.bitmap.width && y >= 0 && y < image.bitmap.height) {
+                    // TODO: Check whether a user is authenticated or not
+                    // TODO: Make sure the user hasn't placed a pixel in the last five minutes
+                    // TODO: Add entry to log
+                    // Update image on server
+                    image.setPixelColor(jimp.cssColorToHex("#" + color), x, y);
+                    image.write("public/canvas.png");
+                    // Announce change via WebSocket
+                    let msg = JSON.stringify({type: 2, x, y, color});
 
-                wss.clients.forEach(sock => {
-                    if(sock.readyState === ws.OPEN) {
-                        sock.send(msg);
-                    }
-                });
+                    wss.clients.forEach(sock => {
+                        if(sock.readyState === ws.OPEN) {
+                            sock.send(msg);
+                        }
+                    });
+                    // We have succeeded!
+                    status.success = true;
+                }
             }
         }
     }
